23a447fbaf1afd407e843208d5fbfd4905445142
Maksims Mitrofanovs <A230458MM@rvt.lv>
2025-03-07 15:10:46 +0200

    Add flip cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 fetch('https://catfact.ninja/fact').then(response => response.json()).then(data => {
     document.getElementById('fact').innerHTML = data.fact;
 });
+
+const cards = document.querySelectorAll('.card__inner');
+cards.forEach(card => {
+    card.addEventListener('click', function() {
+        card.classList.toggle('is-flipped');
+    });
+});
